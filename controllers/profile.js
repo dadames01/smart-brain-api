3a53a44008262b84e735b62120324d246b2b3c83
Daniel Adames <danieladames24@gmail.com>
2022-07-28 10:39:08 -0400

    Upload files

---
+++ controllers/profile.js
@@ -0,0 +1,12 @@
+const profileGet = (req, res, db) => {
+    const { id } = req.params;
+    db.select('*')
+    .from('users')
+    .where({id})
+    .then(user => user.length? res.json(user[0]): res.status(400).json("Not found"))
+    .catch(err => res.json("Error while getting user"));
+}
+
+module.exports = {
+    profileGet
+}
---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const res = require('express/lib/response');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const image = require('./controllers/image');
+const profile = require('./controllers/profile')
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host : '127.0.0.1',
+        user : '',
+        password : '',
+        database : 'smart-brain'
+    }
+})
+  
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+
+app.get('/', (req,res) => {
+    res.send('Succes')
+})
+
+app.post('/signin', (req, res) => {signin.handleSigin(req, res, db, bcrypt)})
+
+app.get('/profile/:id', (req, res) => profile.profileGet(req, res, db));
+
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+
+app.put('/image', (req, res) => image.handleImage(req, res, db));
+app.post('/imageurl', (req, res) => image.handleAPICall(req, res, db));
+
+
+
+
+
+app.listen(3000, () =>{
+    console.log('app is running on port 3000 ')
+})
+
